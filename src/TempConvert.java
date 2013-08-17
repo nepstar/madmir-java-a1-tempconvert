42a2b7f3820f44e6a044337d71372cc011d01b89
nepstar <surajranjit@gmail.com>
2013-08-18 04:48:54 +1200

    Update TempConvert.java

--- src/TempConvert.java
+++ src/TempConvert.java
@@ -6,11 +6,11 @@ public class TempConvert
 		// Fix the errors in this class, to create a working program
 		// Hint: there are TEN errors in total
 		sc = new Scanner(System.in);
-		int tempF; tempC;
+		double tempF; tempC;
 		
-		System.out.print("Enter a temperature in degreez Fahrenheit: ");
-		tempF = sc..nextInt();
-		tempC = (tempF + 32 ) * 5 / 9;
+		System.out.print("Enter a temperature in degrees Fahrenheit: ");
+		tempF = scanner.nextDouble();
+		tempC = (tempF - 32 ) * 5.0 / 9;
 		
 		System.out.println(tempF + " degrees Fahrenheit equals " tempC + " degrees Celcius.");
 	}
